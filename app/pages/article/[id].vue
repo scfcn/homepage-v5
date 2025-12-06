06b172c5d472df047aacd6f1eb8c900052e03d38
筱序二十 <qxbk@qq.com>
2025-12-06 21:23:56 +0800

    feat(文章): 添加文章详情动态路由页面
    实现文章详情页面的基础功能，包括动态路由参数处理和页面导航

---
+++ app/pages/article/[id].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute();
+const id = route.params.id as string;
+
+useHead({ title: `文章: ${id}` });
+definePageMeta({ headerText: "文章详情" });
+</script>
+
+<template>
+  <ZTitle>文章详情</ZTitle>
+  <p>当前访问的文章ID: {{ id }}</p>
+  <p>这是一个动态路由页面，用于处理/article/:id路径。</p>
+  <ZButton @click="navigateTo('/article')"> 返回文章列表 </ZButton>
+</template>
